425c92c337b641e0faa1e3ba6b81ba2ffe378c31
ImBetaRay <lazaro.rodriguez@ciencias.unam.mx>
2022-09-30 15:09:23 -0500

    Se crearon pruebas para dos clases asi como la eliminación de clases todologas

--- src/tests/weatherTests.js
+++ src/tests/weatherTests.js
@@ -1,21 +1,21 @@
 import { Weather } from "../scripts/Weather.js";
 
 function main() {
-    
+    const weather = new Weather('Mi key')
+
+    const objectURLTest = [36.20, 138.25]
+
+    var URLTest =  weather.getRequestURL('mexico')
+    console.log(URLTest)
+
+    var URLTest2 = weather.getRequestURL(objectURLTest)
+    console.log(URLTest2)
+
+    //weather.fetchWeather(URLTest).then(response => console.log(response))
+
+    weather.fetchWeather(URLTest2).then(response => console.log(response))
+
+    //weather.fetchWeather('').then(response => console.log(response))
 }
 
-const weather = new Weather('Mi key')
-
-const objectURLTest = [36.20, 138.25]
-
-var URLTest =  weather.getRequestURL('mexico')
-console.log(URLTest)
-
-var URLTest2 = weather.getRequestURL(objectURLTest)
-console.log(URLTest2)
-
-//weather.fetchWeather(URLTest).then(response => console.log(response))
-
-weather.fetchWeather(URLTest2).then(response => console.log(response))
-
-//weather.fetchWeather('').then(response => console.log(response))
+main()

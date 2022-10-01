692aab80cf5a6ae7151b095e9f404e0550c22f8c
Pablocro <pablocro@ciencias.unam.mx>
2022-09-30 23:47:03 -0500

    Busqueda de KEY funcional y ocultar texto inutil

--- src/scripts/Weather.js
+++ src/scripts/Weather.js
@@ -10,6 +10,21 @@ export class Weather {
     }
 
     /**
+     * Setter para el parametro api que a su vez comprueba que la api sea correcta para las peticiones.
+     * Para comprobar que sea correcto se hace una petición de prueba si es aceptada la nueva key fue correcta
+     * si no se vuelve a la anterior.
+     * @param {string} api el api de nuestro clima.
+     */
+     async setApiKey(apiKey) {
+        if (typeof apiKey != 'string') {
+            throw 'La api no es un string'
+        }
+        var lastKey = this.apikey
+        this.apikey = apiKey;
+        await this.fetchWeather(this.getRequestURL("mexico")).catch((error) => this.apikey = lastKey)
+    }
+
+    /**
      * Función que asigna el objecto que es dado como parametro al atributo dataBase.
      */
     setDataBase(dataBase) {
@@ -17,6 +32,14 @@ export class Weather {
             throw 'No es una base de datos'
         }
         this.dataBase = dataBase
+    }
+
+    /**
+     * 
+     * @returns el apiKey del objeto
+     */
+    getApiKey(){
+        return this.apikey
     }
 
     /**
@@ -35,9 +58,8 @@ export class Weather {
                 location = "lat=" + destination.destination_latitude + 
                            "&lon=" + destination.destination_longitude
         } else {
-            location = 'q=' + city
+            location = 'q=' + city.toUpperCase()
         } 
-          
         return "https://api.openweathermap.org/data/2.5/weather?" + 
                 location + "&units=metric&APPID=" + this.apikey + "&lang=es";
     }
@@ -45,15 +67,15 @@ export class Weather {
     /**
      * Función que dado un URL realiza la petición al api de OpenWeatherMap y 
      * una vez es devuelta, añadé el json con el clima al cache y devuelve la promesa.
-     * @param {String} requestURL EL URL con el que se hará la petición.
+     * @param {string} requestURL EL URL con el que se hará la petición.
      * @returns La promesa que contiene la respuesta de la peticion.
      */
-    fetchWeather(requestURL) {        
+    async fetchWeather(requestURL) {        
           var temp = this.cache.searchByURL(requestURL); 
           if (temp != undefined) {
-            return temp.data
+            return temp.weather
           }
-          return fetch(
+          return await fetch(
             requestURL
             ).then(function(response) {
                 if (!response.ok) {
